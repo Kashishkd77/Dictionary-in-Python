6e8b93df49c1a724bf5f63e10a1cf77bceb8c256
Kashish Dwivedi <84794998+Kashishkd77@users.noreply.github.com>
2021-06-02 04:49:25 +0530

    Update dictionary_functions.py

--- dictionary_functions.py
+++ dictionary_functions.py
@@ -40,13 +40,13 @@ print()
 
 #pop(key[,d]) : Removes the item with the key and returns its value or d if key is not found. If d is not provided and the key is not found, it raises KeyError.
 print("3. pop() method : ")
-dic.pop(1)
+print("Item popped : ",dic.pop(1))
 print("Deleting an element of given key value using pop() : ",dic)
 print()
 
 #popitem() : Removes and returns an arbitrary item (key, value). Raises KeyError if the dictionary is empty.
 print("4. popitem() method : ")
-dic.popitem()
+print("Item popped : ",dic.popitem())
 print("Deleting an arbitrary element using popitem() : ",dic)
 print()
 
@@ -56,10 +56,39 @@ dic.clear()
 print("Deleting all elements using clear() : ",dic)
 print()
 
-#OTHER METHODS OF DICTIONARY :
 #fromkeys(seq[, v])	: Returns a new dictionary with keys from seq and value equal to v (defaults to None).
+print("6. fromkeys(seq[,v]) method : ")
+keys={2,3,5,7}
+value='prime number'
+dict3=dict.fromkeys(keys,value)
+print("Creating new dictionary dict3 using fromkeys() : ",dict3)
+print()
+
 #get(key[,d]) : Returns the value of the key. If the key does not exist, returns d (defaults to None).
+print("7. get(key[,d]) method : ")
+print("Accessing element of key=3 in dict3 using get() : ",dict3.get(3))
+print()
+
 #keys()	: Returns a new object of the dictionary's keys.
+print("8. keys() method : ")
+print("All the keys in dict3 using keys() : ",dict3.keys())
+print()
+
 #setdefault(key[,d]) : Returns the corresponding value if the key is in the dictionary. If not, inserts the key with a value of d and returns d (defaults to None).
+print("9. setdefault(key[,d]) method : ")
+dict1={1:'a',2:'b',3:'c'}
+d1=dict1.setdefault(3)
+d2=dict3.setdefault(4,None)             #Key=4 will be inserted in dict3
+print("Setting default key in dict1 using single argument of setdefault() : ",dict1)
+print("Setting default key in dict3 using both argument of setdefault() : ",dict3)
+print()
+
 #items() : Return a new object of the dictionary's items in (key, value) format.
+print("10. items() method : ")
+print("All elements in dict3 using items() : ",dict3.items())
+print()
+
 #values() : Returns a new object of the dictionary's values
+print("11. values() method : ")
+print("Values of all elements in dict1 using values() : ",dict1.values())
+print()
